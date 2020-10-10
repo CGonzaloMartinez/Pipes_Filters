1899bc45a24a7330bc4963be6568e379d0d54f0a
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2020-10-09 21:00:24 -0300

    Ejercicio 1

--- Repos/Pipes_Filters/src/Program/Program.cs
+++ Repos/Pipes_Filters/src/Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using CompAndDel.Filters;
+using CompAndDel.Pipes;
 
 namespace CompAndDel
 {
@@ -6,6 +8,14 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider p = new PictureProvider();
+            IPicture pic = p.GetPicture("../../matrix.png");
+            FilterGreyscale greyscale = new FilterGreyscale();
+            FilterNegative filternegative = new FilterNegative();
+            PipeNull pipeNull = new PipeNull();
+            PipeSerial pipeserial2 = new PipeSerial(filternegative, pipeNull);
+            PipeSerial pipeSerial = new PipeSerial(greyscale, pipeserial2);
+            pipeSerial.Send(pic);
         }
     }
 }
