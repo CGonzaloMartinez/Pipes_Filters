93a1e7ef65a9f3d83c599714874c19b4126e96fa
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2020-10-11 21:10:57 -0300

    Ejercicio 3 - Agregando TwitterSave

--- Repos/Pipes_Filters/src/Program/Program.cs
+++ Repos/Pipes_Filters/src/Program/Program.cs
@@ -15,20 +15,35 @@ namespace CompAndDel
             PipeNull pipeNull = new PipeNull();
             FilterSave saveFilter = new FilterSave();
 
+            TwitterSave twitterSave = new TwitterSave();
+
             PipeSerial pipeserial3 = new PipeSerial(saveFilter, pipeNull);
+
+            PipeSerial pipeserial4 = new PipeSerial(twitterSave,pipeNull);
+
+            PipeFork pipeFork4 = new PipeFork(pipeNull, pipeserial4);
 
             PipeFork pipeFork3 = new PipeFork(pipeNull, pipeserial3);
 
+
+
+
             PipeSerial pipeserial2 = new PipeSerial(filternegative, pipeFork3);
 
-            PipeFork pipeFork2 = new PipeFork(pipeserial2, pipeserial3);
+            PipeFork pipeFork5 = new PipeFork(pipeserial2,pipeserial4);        
+
+            PipeFork pipeFork2 = new PipeFork(pipeFork5, pipeserial3);
 
             PipeSerial pipeSerial = new PipeSerial(greyscale, pipeFork2);
 
             PipeFork pipeFork1 = new PipeFork(pipeSerial, pipeserial3);
 
 
+
+
+
             pipeFork1.Send(pic);
+
         }
     }
 }
